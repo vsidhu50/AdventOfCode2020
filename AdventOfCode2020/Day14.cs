97ce3a432a19a37c5632f2f53b1b8a78164b496c
vsidhu50 <vsidhu50@gmail.com>
2020-12-15 12:39:38 -0500

    adding days 14/15

---
+++ AdventOfCode2020/Day14.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public class Day14
+    {
+        public Day14()
+        {
+        }
+
+        public static long PartOne()
+        {
+            var lines = Utilities.GetInputLines(14);
+            var mask = "";
+            var mem = new Dictionary<int, long>();
+
+            foreach (var line in lines)
+            {
+                var trimmed = line.Replace(" ", "").Replace("]", "").Replace("[", "");
+                var action = trimmed.Split("=")[0];
+                var result = trimmed.Split("=")[1];
+
+                if (action == "mask")
+                {
+                    mask = result;
+                }
+                else
+                {
+                    var address = Int32.Parse(action.Replace("mem", ""));
+                    var binary = new StringBuilder(Convert.ToString(Int32.Parse(result), 2).PadLeft(36, '0'));
+
+                    for (int i = 0; i < 36; i++)
+                    {
+                        if (mask[i] != 'X')
+                            binary[i] = mask[i];
+                    }
+
+                    mem[address] = Convert.ToInt64(binary.ToString(), 2);
+                }
+            }
+
+            return mem.Values.Sum();
+        }
+
+        public static long PartTwo()
+        {
+            var lines = Utilities.GetInputLines(14);
+            var mask = "";
+            var mem = new Dictionary<long, long>();
+
+            foreach (var line in lines)
+            {
+                var trimmed = line.Replace(" ", "").Replace("]", "").Replace("[", "");
+                var action = trimmed.Split("=")[0];
+                var result = trimmed.Split("=")[1];
+
+                if (action == "mask")
+                {
+                    mask = result;
+                }
+                else
+                {
+                    var address = Int64.Parse(action.Replace("mem", ""));
+                    var binary = new StringBuilder(Convert.ToString(address, 2).PadLeft(36, '0'));
+                    var num = Int64.Parse(result);
+                    var floatingPos = new List<int>();
+
+                    for (int i = 0; i < 36; i++)
+                    {
+                        if (mask[i] == '1')
+                            binary[i] = '1';
+                        else if (mask[i] == 'X')
+                        {
+                            floatingPos.Add(35 - i);
+                            binary[i] = '0';
+                        }
+                    }
+
+                    if (floatingPos.Count == 0)
+                        mem[address] = num;
+                    else
+                    {
+                        var dec = Convert.ToInt64(binary.ToString(), 2);
+                        var memories = new List<long>();
+
+                        AllBin(dec, 0, floatingPos, memories);
+                        foreach (var memory in memories)
+                            mem[memory] = num;
+                    }
+                }
+            }
+
+            return mem.Values.Sum();
+        }
+
+        private static void AllBin(long dec, int index, List<int> floatingPos, List<long> results)
+        {
+            if (index < floatingPos.Count)
+            {
+                if (index == floatingPos.Count - 1)
+                {
+                    results.Add(dec);
+                    results.Add(dec + (long)Math.Pow(2, floatingPos[index]));
+                }
+                else
+                {
+                    AllBin(dec, index + 1, floatingPos, results);
+                    AllBin(dec + (long)Math.Pow(2, floatingPos[index]), index + 1, floatingPos, results);
+                }
+            }
+        }
+    }
+    
+}
---
+++ AdventOfCode2020/Day15.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public class Day15
+    {
+        public Day15()
+        {
+        }
+
+        public static int PartOne()
+        {
+            var input = new List<int> { 1,0,15,2,10,13 };
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 1; i < input.Count; i++)
+            {
+                numbers[input[i - 1]] = i;
+            }
+
+            var last = input[^1];
+            var turn = input.Count + 1;
+
+            while (turn < 2021)
+            {
+                if (!numbers.ContainsKey(last))
+                {
+                    numbers[last] = turn - 1;
+                    last = 0;
+                }
+                else
+                {
+                    var diff = turn - 1 - numbers[last];
+                    numbers[last] = turn - 1;
+                    last = diff;
+                }
+
+                turn++;
+            }
+
+            return last;
+        }
+
+        public static int PartTwo()
+        {
+            var input = new List<int> { 1, 0, 15, 2, 10, 13 };
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 1; i < input.Count; i++)
+            {
+                numbers[input[i - 1]] = i;
+            }
+
+            var last = input[^1];
+            var turn = input.Count + 1;
+
+            while (turn < 30000001)
+            {
+                if (!numbers.ContainsKey(last))
+                {
+                    numbers[last] = turn - 1;
+                    last = 0;
+                }
+                else
+                {
+                    var diff = turn - 1 - numbers[last];
+                    numbers[last] = turn - 1;
+                    last = diff;
+                }
+
+                turn++;
+            }
+
+            return last;
+        }
+    }
+}
